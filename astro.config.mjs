70ba38e545b88b53ba7f32b64fd01850d10fb7d3
lmadrigal <lmadrigal@goacegroup.com>
2026-09-19 15:24:20 -0600

    node update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'hybrid',
   adapter: vercel()
 });
